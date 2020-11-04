9f176e0441572e72f34c3ad5bfa1c4279c58efb6
walkersutton <walker.c.sutton@gmail.com>
2020-11-04 16:34:36 -0500

    Initial Commit

---
+++ redeye/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  redeye
+//
+//  Created by Walker Cole Sutton on 11/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
