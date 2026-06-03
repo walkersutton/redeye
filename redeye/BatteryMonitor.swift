897a84aa81ad6eb01f68d6a89d68c851bb830ad9
Walker Sutton <walker.c.sutton@gmail.com>
2026-06-03 14:58:31 -0700

    instant flush on charge

--- redeye/BatteryMonitor.swift
+++ redeye/BatteryMonitor.swift
@@ -12,9 +12,18 @@ class BatteryMonitor: ObservableObject {
     @Published private(set) var state = BatteryState(percentage: nil, isCharging: false, timeToEmptyMinutes: nil)
 
     private var timer: Timer?
+    private var powerSourceRunLoopSource: CFRunLoopSource?
+
+    deinit {
+        timer?.invalidate()
+        if let powerSourceRunLoopSource {
+            CFRunLoopRemoveSource(CFRunLoopGetMain(), powerSourceRunLoopSource, .commonModes)
+        }
+    }
 
     func start() {
         refresh()
+        startPowerSourceNotifications()
         let t = Timer(timeInterval: 30, repeats: true) { [weak self] _ in self?.refresh() }
         RunLoop.main.add(t, forMode: .common)
         timer = t
@@ -22,6 +31,20 @@ class BatteryMonitor: ObservableObject {
 
     func refresh() {
         state = readState()
+    }
+
+    private func startPowerSourceNotifications() {
+        guard powerSourceRunLoopSource == nil else { return }
+
+        let context = Unmanaged.passUnretained(self).toOpaque()
+        let source = IOPSNotificationCreateRunLoopSource({ context in
+            guard let context else { return }
+            let monitor = Unmanaged<BatteryMonitor>.fromOpaque(context).takeUnretainedValue()
+            monitor.refresh()
+        }, context).takeRetainedValue()
+
+        CFRunLoopAddSource(CFRunLoopGetMain(), source, .commonModes)
+        powerSourceRunLoopSource = source
     }
 
     private func readState() -> BatteryState {
@@ -38,11 +61,12 @@ class BatteryMonitor: ObservableObject {
             let current = info[kIOPSCurrentCapacityKey] as? Int ?? 0
             let max = info[kIOPSMaxCapacityKey] as? Int ?? 100
             let charging = info[kIOPSIsChargingKey] as? Bool ?? false
+            let isPluggedIn = charging || (info[kIOPSPowerSourceStateKey] as? String) == kIOPSACPowerValue
             let tte = info[kIOPSTimeToEmptyKey] as? Int
 
             return BatteryState(
                 percentage: max > 0 ? (current * 100) / max : 0,
-                isCharging: charging,
+                isCharging: isPluggedIn,
                 timeToEmptyMinutes: (tte ?? -1) > 0 ? tte : nil
             )
         }
